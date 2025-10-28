8dc8e7a8914b4f09d7e2288922131312cbff84d1
kerosenecollab <Rogkov_P@mail.ru>
2025-10-28 11:26:42 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import telebot
+import json, os
+from telebot import types
+
+token = os.environ.get('8308403784:AAHjvJJmgq5xUaBR_Fa-Aw39qFF101vt6yM')
+bot = telebot.TeleBot(token)
+
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.InlineKeyboardMarkup()
+
+    btn1 = types.InlineKeyboardButton(text='Понедельник', callback_data='monday')
+    btn2 = types.InlineKeyboardButton(text='Вторник', callback_data='tuesday')
+    btn3 = types.InlineKeyboardButton(text='Среда', callback_data='wednesday')
+    btn4 = types.InlineKeyboardButton(text='Четверг', callback_data='thursday')
+    btn5 = types.InlineKeyboardButton(text='Пятница', callback_data='friday')
+    btn6 = types.InlineKeyboardButton(text='Суббота', callback_data='saturday')
+
+    markup.row(btn1, btn2)
+    markup.row(btn3, btn4)
+    markup.row(btn5, btn6)
+
+    bot.send_message(message.chat.id, 'Выберите день', reply_markup=markup)
+
+
+
+@bot.callback_query_handler(func= lambda call: True)
+def days(call):
+    with open('schedule.json', 'r') as file:
+        data = json.load(file)
+    day = call.data
+    day_lessons = data[day]
+
+    text = f"🥲{day.capitalize()}:\n"
+
+    for lesson in day_lessons:
+        text += f"{lesson['subject']} в {lesson['time']} - {lesson['class']}\n"
+    bot.send_message(call.message.chat.id, text)
+    
+
+bot.polling(none_stop=True)
+
+
+
+
+
+
+
+
+
